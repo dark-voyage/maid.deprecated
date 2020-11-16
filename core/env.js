544f9bc04c3f922a702fd16c9fa39718dc06553b
Geno Ferollo <genemators@gmail.com>
2020-11-16 22:14:11 +0500

    Env typed

--- core/env.js
+++ core/env.js
@@ -1,3 +1,9 @@
+/**
+ * @name Process Env Shortener & Loader
+ * @description Shorten all env structures and load up .env file if exists
+ * @module {process}
+ */
+
 require("dotenv").config();
 
 module.exports = process.env;
